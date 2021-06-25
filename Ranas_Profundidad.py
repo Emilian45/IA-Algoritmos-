e86e910ca7d5f531d442707a380dcb7d0a7141af
Emilian45 <emilian.muppers@gmail.com>
2021-06-24 20:23:54 -0500

    Identacion y correcion
    Identado
    True con mayusculas
    Agregado tiempo de resolucion con import time, inicializando varibale antes de  la ejecucion y despues de

--- Ranas_Profundidad.py
+++ Ranas_Profundidad.py
@@ -1,4 +1,5 @@
 import copy
+import time
 
 
 '''
@@ -21,7 +22,7 @@ Definimos la comparacion que se realizara de los elementos dentro de losa rreglo
 def EstadoFEncontrado(estado_inicial,estado_final):
     for i in range(len(estado_inicial)):
         if estado_inicial[i] == estado_final[i]:
-            return true
+          return True
     return False
 '''
 Definimos una funcion que nos retornasra el valore de i el cual sera el lugar del espacio vacio encontrado (roca vacia)
@@ -32,55 +33,53 @@ def busquedaEspacioVacio(estado_inicial):
             return i
             break
 
+'''
+dado el indice del espacio vacio cerificara si el sapo esta en la posicion para moverse
+y se copiara el estado en el que se encuentra y mandarlo dentro de la lista recursiva que verificara si es el estado final
+y si no lo es hara de nuevo la busqueda entr los posibles movimientos, asi en u  ciclo hasta que  encuentre
+'''
 def busquedaProfundidad(estado_inicial,estado_final):
-    List.append(estado_inicial) #Comenzamos agregando el estado inicial a la lista para tener nuestro primer nodo
-    if EstadoFEncontrado(estado_inicial,estado_final):
-        return True #Si encuentras la solucion ya salte amigo------------------------------------
-    else:
-        i=busquedaEspacioVacio(estado_inicial) #Si no haras los siguientes pasos para resolver el problema de la vida----------
-        
-    '''
-    dado el indice del espacio vacio cerificara si el sapo esta en la posicion para moverse
-    y se copiara el estado en el que se encuentra y mandarlo dentro de la lista recursiva que verificara si es el estado final
-    y si no lo es hara de nuevo la busqueda entr los posibles movimientos, asi en u  ciclo hasta que  encuentre
-    '''
-        if i+1 <= len(estado_inicial)-1 and estado_inicial[i+1] == "S":
-            copiaestado = copy.copy(estado_inicial)
-            copiaestado[i] = "S"
-            copiaestado[i+1] = "V"
-            if busquedaProfundidad (copiaestado, estado_final): #Aplicamos la recursividad con la copia del estado inicial para elarguemnto quede en el estado actual
-                return True
+  List.append(estado_inicial) #Comenzamos agregando el estado inicial a la lista para tener nuestro primer nodo
+  if EstadoFEncontrado(estado_inicial,estado_final):
+      return True #Si encuentras la solucion ya salte amigo------------------------------------
+  else:
+      i=busquedaEspacioVacio(estado_inicial) #Si no haras los siguientes pasos para resolver el problema de la vida---------
+      if i+1 <= len(estado_inicial)-1 and estado_inicial[i+1] == "S":
+        copiaestado = copy.copy(estado_inicial)
+        copiaestado[i] = "S"
+        copiaestado[i+1] = "V"
+        if busquedaProfundidad (copiaestado, estado_final): #Aplicamos la recursividad con la copia del estado inicial para elarguemnto quede en el estado actual
+          return True
 
-        if i-2 >= 0 and estadoinicial[i-2] == "R":
-            copiaestado = copy.copy(estadoinicial)
-            copiaestado[i] = "R"
-            copiaestado[i-2] = "V"
-            if busquedaProfundidad(copiaestado,estadofinal):
-            return True
+      if i-2 >= 0 and estadoinicial[i-2] == "R":
+        copiaestado = copy.copy(estadoinicial)
+        copiaestado[i] = "R"
+        copiaestado[i-2] = "V"
+        if busquedaProfundidad(copiaestado,estadofinal):
+          return True
 
-        if i+2 <= len(estadoinicial)-1 and estadoinicial[i+2] == "S":
-            copiaestado = copy.copy(estadoinicial)
-            copiaestado[i] = "S"
-            copiaestado[i+2] = "V"
-            if busquedaProfundidad(copiaestado,estadofinal):
-            return True
+      if i+2 <= len(estadoinicial)-1 and estadoinicial[i+2] == "S":
+        copiaestado = copy.copy(estadoinicial)
+        copiaestado[i] = "S"
+        copiaestado[i+2] = "V"
+        if busquedaProfundidad(copiaestado,estadofinal):
+           return True
 
-        if i-1 >= 0 and estadoinicial[i-1] == "R":
-            copiaestado = copy.copy(estadoinicial)
-            copiaestado[i] = "R"
-            copiaestado[i-1] = "V"
-            if busquedaProfundidad(copiaestado,estadofinal):
+      if i-1 >= 0 and estadoinicial[i-1] == "R":
+          copiaestado = copy.copy(estadoinicial)
+          copiaestado[i] = "R"
+          copiaestado[i-1] = "V"
+          if busquedaProfundidad(copiaestado,estadofinal):
             return True
-        List.pop()
-        return False
-    else:
-         return True
+      List.pop()
+      return False
+
+profundidad_inicio = time.time()
 
 if busquedaProfundidad(estInicio,estFinal):
   print("Solución Encontrada")
   for i in lista:
     print(i)
 
-        
-            
-
+profundidad_final= time.time()
+print("\nBúsqueda finalizada en",profundidad_final - profundidad_inicio,"segundos\n")
