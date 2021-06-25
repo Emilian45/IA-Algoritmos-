4cb8fe85862f254b74d6171fdfb380faa42927b6
Emilian45 <emilian.muppers@gmail.com>
2021-06-24 20:58:45 -0500

    Estado inicial comentado
    Manejaremos una lista de listas

---
+++ Lobo_Oveja_Coles_profundidad.py
@@ -0,0 +1,17 @@
+import copy
+import time
+
+
+'''
+Mapa con un barquero, 1 barco y 1 oveja, 1 caja de coles, 1 lobo y dos orillas
+separadas por un lago
+Donde  el barquero y el barco seran el cambio  entre la lista de listas
+Cada lista dentro de la listra representa una orilla en donde el objetivo es pasar  todos los objetos de una lista a otra lista dentro de la lista 
+siguiendo las reglas que se estreblcerean mas adelantes
+'''
+estInicio = [["Lobo", "Coles", "Oveja"],[]]
+List=[]
+
+
+
+
