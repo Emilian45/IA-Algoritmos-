4115e37976f67d2f3bb870b8219f1ea583317423
Emilian45 <emilian.muppers@gmail.com>
2021-06-25 22:58:49 -0500

    Reutilizar codigo de la barquero

--- Canibales_Misioneros_Profundidad.py
+++ Canibales_Misioneros_Profundidad.py
@@ -10,4 +10,29 @@ def EstadoFEncontrado(estado_inicial):
     elif len(estado_inicial)==6:
         return True
 
-def
+def BusquedaCanibal(Orilla):
+  Canibal  = 0
+  for i in Orilla:
+    if i == "Canibal":
+      Canibal += 1 
+  return Canibal
+
+def BusquedaMisionero(Orilla):
+  Misionero = 0
+  for i in Orilla:
+    if i == "Misionero":
+      Misionero += 1
+  return Misionero  
+
+Prof = 20 #La maxima cantidad de estados aceptar en nuestra cola
+
+def busquedaProfundidad(estado_inicial):
+    List.append(estado_inicial)
+    orilla1= List[0]
+    orilla2= List[1]
+
+    if EstadoFEncontrado(estado_inicial):
+        return True
+  
+    else:
+        if len(List)-1 < Prof and len(orilla2)>0:
