8f621fe0a1d381a42df2b5ee0f333be97b318992
Emilian45 <emilian.muppers@gmail.com>
2021-06-25 23:29:59 -0500

    Primeras dos condiciones
    Ya cerca de dormir por hoy pero ya casi 2.5/5

--- Canibales_Misioneros_Profundidad.py
+++ Canibales_Misioneros_Profundidad.py
@@ -36,3 +36,25 @@ def busquedaProfundidad(estado_inicial):
   
     else:
         if len(List)-1 < Prof and len(orilla2)>0:
+            #Ya que se aplicara de nuevo el modo de vuelta ida tendremos que ver de ambos lados 
+            if BusquedaCanibal(orilla1) >= 2 and BusquedaMisionero(orilla1) - BusquedaCanibal(orilla1) >=0 and BusquedaMisionero(orilla1) - BusquedaCanibal(orilla2) >=2 or BusquedaCanibal(orilla1) >= 2 and miBusquedaMisionerosioneros(orilla1) - BusquedaCanibal(orilla1) >=0 and BusquedaMisionero(orilla2) == 0: 
+                copia_orilla1= copy.copy(orilla1)
+                copia_orilla2= copy.copy(orilla2)
+                copia_orilla1.remove("Canibal")
+                copia_orilla1.remove("Canibal")
+                copia_orilla2.remove("Canibal")
+                copia_orilla2.remove("Canibal")
+                if busquedaProfundidad( [ orilla1 , orilla2,"Mandamos 2 canibales" ] ):
+                    return True 
+                    
+        if len(List)-1 < Prof and len(orilla2)>0:
+            #Ya que se aplicara de nuevo el modo de vuelta ida tendremos que ver de ambos lados 
+            if BusquedaCanibal(orilla1) >= 2 and BusquedaMisionero(orilla1) - BusquedaCanibal(orilla1) >=0 and BusquedaMisionero(orilla1) - BusquedaCanibal(orilla2) >=2 or BusquedaCanibal(orilla1) >= 2 and miBusquedaMisionerosioneros(orilla1) - BusquedaCanibal(orilla1) >=0 and BusquedaMisionero(orilla2) == 0: 
+                copia_orilla1= copy.copy(orilla1)
+                copia_orilla2= copy.copy(orilla2)
+                copia_orilla1.remove("Canibal")
+                copia_orilla1.remove("Canibal")
+                copia_orilla2.remove("Canibal")
+                copia_orilla2.remove("Canibal")
+                if busquedaProfundidad( [ orilla1 , orilla2,"Mandamos 2 canibales" ] ):
+                    return True 
