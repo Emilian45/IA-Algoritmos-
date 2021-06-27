c7ed408f78b0b586bd1e658d6b366a4ea5d41d0d
Emilian45 <emilian.muppers@gmail.com>
2021-06-27 15:57:45 -0500

    Chorrocientos posibles movs
    Poner el retorno individual sino como chinga?

--- Familia_Puente.py
+++ Familia_Puente.py
@@ -31,6 +31,7 @@ def busquedaProfundidad(estado_inicial, luz):
     else:
 
         if len(List)-1 < Prof and len(orilla1)>0 and luz >0:
+            #Movimientos con el 1
             #Velocidad del 3
             if  BuscaExsitenciaSujeto(orilla1,1) and BuscaExsitenciaSujeto(orilla1,3)
                 copia_orilla1 = copy.copy(orilla1)
@@ -50,8 +51,99 @@ def busquedaProfundidad(estado_inicial, luz):
                 copia_orilla1.remove(6)
                 copia_orilla2.append(1)
                 copia_orilla2.append(6)
-                copia_luz= copy.copy(luz)-3
+                copia_luz= copy.copy(luz)-6
                 if busquedaProfundidad([copia_orilla1],[copia_orilla2,"Luz menos 6"],copia_luz)
                     return True
 
+            elif  BuscaExsitenciaSujeto(orilla1,1) and BuscaExsitenciaSujeto(orilla1,8)
+                copia_orilla1 = copy.copy(orilla1)
+                copia_orilla2 = copy.copy(orilla2)
+                copia_orilla1.remove(1)
+                copia_orilla1.remove(8)
+                copia_orilla2.append(1)
+                copia_orilla2.append(8)
+                copia_luz= copy.copy(luz)-8
+                if busquedaProfundidad([copia_orilla1],[copia_orilla2,"Luz menos 8"],copia_luz)
+                    return True
+            
+            elif  BuscaExsitenciaSujeto(orilla1,1) and BuscaExsitenciaSujeto(orilla1,12)
+                copia_orilla1 = copy.copy(orilla1)
+                copia_orilla2 = copy.copy(orilla2)
+                copia_orilla1.remove(1)
+                copia_orilla1.remove(12)
+                copia_orilla2.append(1)
+                copia_orilla2.append(12)
+                copia_luz= copy.copy(luz)-12
+                if busquedaProfundidad([copia_orilla1],[copia_orilla2,"Luz menos 12"],copia_luz)
+                    return True
 
+
+            #Movimientos  con el 2 mas rapido
+            elif  BuscaExsitenciaSujeto(orilla1,3) and BuscaExsitenciaSujeto(orilla1,6)
+                copia_orilla1 = copy.copy(orilla1)
+                copia_orilla2 = copy.copy(orilla2)
+                copia_orilla1.remove(3)
+                copia_orilla1.remove(6)
+                copia_orilla2.append(3)
+                copia_orilla2.append(6)
+                copia_luz= copy.copy(luz)-6
+                if busquedaProfundidad([copia_orilla1],[copia_orilla2,"Luz menos 6"],copia_luz)
+                    return True
+            
+            elif  BuscaExsitenciaSujeto(orilla1,3) and BuscaExsitenciaSujeto(orilla1,8)
+                copia_orilla1 = copy.copy(orilla1)
+                copia_orilla2 = copy.copy(orilla2)
+                copia_orilla1.remove(3)
+                copia_orilla1.remove(8)
+                copia_orilla2.append(3)
+                copia_orilla2.append(8)
+                copia_luz= copy.copy(luz)-8
+                if busquedaProfundidad([copia_orilla1],[copia_orilla2,"Luz menos 8"],copia_luz)
+                    return True
+
+            elif  BuscaExsitenciaSujeto(orilla1,3) and BuscaExsitenciaSujeto(orilla1,12)
+                copia_orilla1 = copy.copy(orilla1)
+                copia_orilla2 = copy.copy(orilla2)
+                copia_orilla1.remove(3)
+                copia_orilla1.remove(12)
+                copia_orilla2.append(3)
+                copia_orilla2.append(12)
+                copia_luz= copy.copy(luz)-12
+                if busquedaProfundidad([copia_orilla1],[copia_orilla2,"Luz menos 12"],copia_luz)
+                    return True
+
+            #Movimientos con el tercero mas rapido 
+
+            elif  BuscaExsitenciaSujeto(orilla1,6) and BuscaExsitenciaSujeto(orilla1,8)
+                copia_orilla1 = copy.copy(orilla1)
+                copia_orilla2 = copy.copy(orilla2)
+                copia_orilla1.remove(6)
+                copia_orilla1.remove(8)
+                copia_orilla2.append(6)
+                copia_orilla2.append(8)
+                copia_luz= copy.copy(luz)-8
+                if busquedaProfundidad([copia_orilla1],[copia_orilla2,"Luz menos 8"],copia_luz)
+                    return True
+            
+            elif  BuscaExsitenciaSujeto(orilla1,6) and BuscaExsitenciaSujeto(orilla1,12)
+                copia_orilla1 = copy.copy(orilla1)
+                copia_orilla2 = copy.copy(orilla2)
+                copia_orilla1.remove(6)
+                copia_orilla1.remove(12)
+                copia_orilla2.append(6)
+                copia_orilla2.append(12)
+                copia_luz= copy.copy(luz)-12
+                if busquedaProfundidad([copia_orilla1],[copia_orilla2,"Luz menos 12"],copia_luz)
+                    return True
+            #Movimientos con el cuarto
+            elif  BuscaExsitenciaSujeto(orilla1,8) and BuscaExsitenciaSujeto(orilla1,12)
+                copia_orilla1 = copy.copy(orilla1)
+                copia_orilla2 = copy.copy(orilla2)
+                copia_orilla1.remove(8)
+                copia_orilla1.remove(12)
+                copia_orilla2.append(8)
+                copia_orilla2.append(12)
+                copia_luz= copy.copy(luz)-12
+                if busquedaProfundidad([copia_orilla1],[copia_orilla2,"Luz menos 12"],copia_luz)
+                    return True
+                    
