cc7b59f7102c70015943f69747ad1c2b8a6c78ca
Emilian45 <emilian.muppers@gmail.com>
2021-06-29 14:26:21 -0500

    Jalooooooooooo Familia_Puente
    No se porque no me indicaba error colab, pero faile unas cosas adrede y tenias error muy pendejos de sintaxis pero nice jalo, queda revision a logica y comentarios detallados

--- Familia_Puente.py
+++ Familia_Puente.py
@@ -23,7 +23,7 @@ def OnePiece(estado_actual, estado_final):
 Prof = 20 #La maxima cantidad de estados aceptar en nuestra lista que trabaja como pila :p
 
 def busquedaProfundidad(estado_actual, estado_final, sentido, tiempo ):
-    if len(List)-1 < Prof and tiempo >0
+    if len(List)-1 < Prof and tiempo >0:
         List.append(estado_actual)
         orilla1 = estado_actual[0]
         orilla2= estado_actual[1]
@@ -32,55 +32,51 @@ def busquedaProfundidad(estado_actual, estado_final, sentido, tiempo ):
             return True
         else:
 
-            if sentido == True:
-                cambiosentido= False
+            if sentido == False:
+                cambiosentido= True
+                
                 for x in orilla1:
-                    copia_orilla1= copy.copy(origen)
+                    copia_orilla1= copy.copy(orilla1)
                     copia_orilla1.remove(x)
 
-                    if len(orilla1) > 0:
+                    if len(copia_orilla1) > 0:
                         for y in copia_orilla1:
 
                             copia2_orilla1= copy.copy(orilla1)
                             copia_orilla2= copy.copy(orilla2)
-                            copia2_orilla1=remove(x)
-                            copia2_orilla1=remove(y)
-                            copia_orilla2= append(x)
-                            copia_orilla2= append(y)
-                            copia_tiempo = copu.copy(tiempo)
+                            copia2_orilla1.remove(x)
+                            copia2_orilla1.remove(y)
+                            copia_orilla2.append(x)
+                            copia_orilla2.append(y)
+                            copia_tiempo = copy.copy(tiempo)
                             if x-y > 0:
                                 copia_tiempo = copia_tiempo - x
-                                if busquedaProfundidad([copia2_orilla1,copia_orilla2, "Tiempo Restante: ", copia_tiempo], estado_final, cambiosentido, copia_tiempo)
-                                return True
+                                if busquedaProfundidad([copia2_orilla1,copia_orilla2, "Tiempo Restante: ", copia_tiempo], estado_final, cambiosentido, copia_tiempo):
+                                  return True
                             else:
                                 copia_tiempo = copia_tiempo - y
-                                if busquedaProfundidad([copia2_orilla1,copia_orilla2, "Tiempo Restante: ", copia_tiempo], estado_final, cambiosentido, copia_tiempo)
-                                return True
+                                if busquedaProfundidad([copia2_orilla1,copia_orilla2, "Tiempo Restante: ", copia_tiempo], estado_final, cambiosentido, copia_tiempo):
+                                  return True
 
-            if sentido == False:
-                cambiosentido= True
+            if sentido == True:
+                cambiosentido= False
                 for x in orilla2:
                     copia_orilla1=copy.copy(orilla1)
                     copia_orilla2=copy.copy(orilla2)
-                    copia_orilla2.remove(a)
-                    copia_orilla1.append(a)
+                    copia_orilla2.remove(x)
+                    copia_orilla1.append(x)
                     copia_tiempo = copy.copy(tiempo)
-                    copia_tiempo= copia_tiempo - a
+                    copia_tiempo= copia_tiempo - x
                     if busquedaProfundidad ([copia_orilla1,copia_orilla2, "Tiempo Restante: ", copia_tiempo], estado_final, cambiosentido, copia_tiempo):
                         return True
                     
             List.pop()
             return False
     return False
-
-if   busquedaProfundidad(estInicio, estFinal,Ida, lampara):
+if busquedaProfundidad(estInicio, estFinal,Ida, lampara):
     print("Llegaste a Laugh Tale")
     for i in List:
         print(i)
-
-
-
-                        
 
 
 
