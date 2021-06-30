5224de5f570abe759be1dfc0636a4a42664e102b
Emilian45 <emilian.muppers@gmail.com>
2021-06-30 17:52:13 -0500

    Funciones que tome lobo

--- Familia_Ladron.py
+++ Familia_Ladron.py
@@ -4,3 +4,19 @@ import time
 
 estInicio= [["Padre","Hijo","Hijo","Madre","Hija","Hija","Policia","Cholo"],[]]
 estFinal=[[],["Padre","Hijo","Hijo","Madre","Hija","Hija","Policia","Cholo"]]
+List=[]
+Ida= False
+
+
+def OnePiece(estado_actual, estado_final):
+    if len(estado_actual)==estado_final:
+        return True
+    else:
+        return False
+
+def validacionExisObjeto(Orilla, Objeto):
+  for i in Orilla:
+    if i == Objeto:
+      return True
+  else:
+    return False
