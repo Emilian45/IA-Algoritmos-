cea41a643913bbafdc3adb4c42840eed5f4d7bed
Emilian45 <emilian.muppers@gmail.com>
2021-06-30 17:46:00 -0500

    Estados iniciales

--- Familia_Ladron.py
+++ Familia_Ladron.py
@@ -1,1 +1,6 @@
 #Comenzamos con esta wea
+import copy
+import time
+
+estInicio= [["Padre","Hijo","Hijo","Madre","Hija","Hija","Policia","Cholo"],[]]
+estFinal=[[],["Padre","Hijo","Hijo","Madre","Hija","Hija","Policia","Cholo"]]
