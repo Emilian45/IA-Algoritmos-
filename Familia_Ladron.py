f1f519071fcfe46e2955f1037365b4e9dcceed54
Emilian45 <emilian.muppers@gmail.com>
2021-07-02 20:22:50 -0500

    Identacion y cambio de Barbanegra

--- Familia_Ladron.py
+++ Familia_Ladron.py
@@ -1,117 +1,120 @@
-#Comenzamos con esta wea
 import copy
 import time
 
-'''
-Un mapa con dos orillas, un río con barca 1 ladrón, 1 policía, 1 padre, 1
-madre, 2 niñas y 2 niños
-'''
-estInicio= [["Padre","Hijo","Hijo","Madre","Hija","Hija","Policia","Cholo"],[]]
-estFinal=[[],["Padre","Hijo","Hijo","Madre","Hija","Hija","Policia","Cholo"]]
-List=[]
-Ida= False
+estInicio = [["Policia","Ladron","Padre","Hijo","Hijo","Madre","Hija","Hija"],[]]
+estFinal   = [[],["Policia","Ladron","Padre","Hijo","Hijo","Madre","Hija","Hija"]]
+profundidad = 20
+ida = False
+List = []
 
+def OnePiece(estado_inicial,estado_final):
+  if len(estado_inicial) == len(estado_final):
+    return True
+  return False
 
-def OnePiece(estado_actual, estado_final):
-    if len(estado_actual)==estado_final:
-        return True
-    else:
-        return False
-
-def validacionExisObjeto(Orilla, Objeto):
+def validacionExisObjeto(Orilla,Objeto):
   for i in Orilla:
     if i == Objeto:
       return True
-  else:
-    return False
+  return False
 
-def reglas(estado_actual,sentido):
-    orilla1=estado_actual[0]
-    orilla2=estado_actual[1]
-    totalel = len(orilla1) + len(orilla2)
+def reglas(estado_inicial,sentido):
+  orilla1  = estado_inicial[0]
+  orilla2 = estado_inicial[1]
+  NoElementos = len(orilla1) + len(orilla2)
+  '''
+  Primero guardamos los arreglos orilla1 y orilla2 de nuestro estado actual en  variables,
+  Añadimos una variable que va a contener el numero total de elementos que tiene nuestro
+  estado actual, despues indicamos que vlide cualquier otro estado que no sea el inicial
+  '''
+  if len(orilla1) > 0 and len(orilla2) > 0 or len(orilla1) == 0 and len(orilla2) == NoElementos:
+    '''
+    El valor 1 en direc indica que el movimiento realizado fue orilla1 -> orilla2
+    despues validamos que el elemento que el ultimo elemento que paso  fuese un Padre, Madre o Policia,
+    de no serlo retornará un False 
+    '''
+    if sentido == 1:
+      if orilla2[len(orilla2) - 1] == "Hijo" or orilla2[len(orilla2) - 1] == "Hija" or orilla2[len(orilla2) - 1] == "Ladron":
+        return False
 
-    if len(orilla1) > 0 and len(orilla2) > 0 or len(orilla1) == 0 and len(orilla2) == totalel:
+    if sentido == 0:
+      if orilla1[len(orilla1) - 1] == "Hijo" or orilla1[len(orilla1) - 1] == "Hija" or orilla1[len(orilla1) - 1] == "Ladron":
+        return False
+    '''
+    Validamos que ningun miembro se quede solo con el ladron sin la presencia del policia 
+    '''
+    if len(orilla1) >= 2 and validacionExisObjeto(orilla1,"Ladron") and not validacionExisObjeto(orilla1,"Policia") or len(orilla2) >= 2 and validacionExisObjeto(orilla2,"Ladron") and not validacionExisObjeto(orilla2,"Policia"):
+      return False
+    '''
+    Validamos que ningun hijo se quede solo con la Madre sin la presencia del Padre 
+    '''
+    if validacionExisObjeto(orilla1,"Hijo") and validacionExisObjeto(orilla1,"Madre") and not validacionExisObjeto(orilla1,"Padre") or validacionExisObjeto(orilla2,"Hijo") and validacionExisObjeto(orilla2,"Madre") and not validacionExisObjeto(orilla2,"Padre"):
+        return False
+    '''
+    Validamos que ninguna Hija se quede solo con el Padre sin la presencia de la Madre 
+    '''
+    if validacionExisObjeto(orilla1,"Hija") and validacionExisObjeto(orilla1,"Padre") and not validacionExisObjeto(orilla1,"Madre") or validacionExisObjeto(orilla2,"Hija") and validacionExisObjeto(orilla2,"Padre") and not validacionExisObjeto(orilla2,"Madre"):
+        return False
 
-        if sentido == True:
-            if orilla2[len(orilla2) - 1] == "Hijo" or orilla2[len(orilla2) - 1] == "Hija" or orilla2[len(orilla2) - 1] == "Cholo":
-                return False
-
-        if sentido == False:
-            if orilla1[len(orilla1) - 1] == "Hijo" or orilla1[len(orilla1) - 1] == "Hija" or orilla1[len(orilla1) - 1] == "Cholo":
-                return False
-        '''
-        Validamos que ningun miembro se quede solo con el orilla2 sin la presencia del policia 
-        '''
-        if len(orilla1) >= 2 and validacionExisObjeto(orilla1,"Cholo") and not validacionExisObjeto(orilla1,"Policia") or len(orilla2) >= 2 and validacionExisObjeto(orilla2,"Cholo") and not validacionExisObjeto(orilla2,"Policia"):
-            return False
-        '''
-        Validamos que ningun hijo se quede solo con la Madre sin la presencia del Padre 
-        '''
-        if validacionExisObjeto(orilla1,"Hijo") and validacionExisObjeto(orilla1,"Madre") and not validacionExisObjeto(orilla1,"Padre") or validacionExisObjeto(orilla2,"Hijo") and validacionExisObjeto(orilla2,"Madre") and not validacionExisObjeto(orilla2,"Padre"):
-            return False
-        '''
-        Validamos que ninguna Hija se quede solo con el Padre sin la presencia de la Madre 
-        '''
-        if validacionExisObjeto(orilla1,"Hija") and validacionExisObjeto(orilla1,"Padre") and not validacionExisObjeto(orilla1,"Madre") or validacionExisObjeto(orilla2,"Hija") and validacionExisObjeto(orilla2,"Padre") and not validacionExisObjeto(orilla2,"Madre"):
-            return orilla1
-
-    return True
+  return True
 
 Prof=20
 
-def busquedaProfundidad(estado_actual, estado_final, sentido, abajo):
-    if len(List)-1 < Prof and reglas(estado_actual, abajo):
-        List.append(estado_actual)
-        orilla1= estado_actual[0] 
-        orilla2= estado_actual[1]
-        copia_profundidad =copy.copy(abajo)
+def busquedaProfundidad(estado_inicial,estado_final,sentido,prof):
+  if len(List)-1 < Prof:
+    if reglas(estado_inicial,sentido):
+      copia_profundidad = copy.copy(prof)
+      copia_profundidad -= 1
+      List.append(estado_inicial)
+      orilla1  = estado_inicial[0]
+      orilla2 = estado_inicial[1]
+      ef      = estado_final[1]
+      if OnePiece(orilla2,ef):
+        return True
+      else:
+        if sentido == False:
+          cambiosentido = True
+          for x in orilla1:
+            copia_orilla1 = copy.copy(orilla1)
+            copia_orilla1.remove(x)
+            for y in copia_orilla1:
+              copia2_orilla1 = copy.copy(orilla1)
+              copia_orilla2 = copy.copy(orilla2)
+              copia2_orilla1.remove(x)
+              copia2_orilla1.remove(y)
+              copia_orilla2.append(x)
+              copia_orilla2.append(y)
+              if busquedaProfundidad([copia2_orilla1,copia_orilla2,"Enviamos: ",x,y],estado_final,cambiosentido,copia_profundidad):
+                return True
+        if sentido == True:
+          cambiosentido = False
+          for x in orilla2:
+            copia_orilla1 = copy.copy(orilla1)
+            copia_orilla2 = copy.copy(orilla2)
+            copia_orilla2.remove(x)
+            copia_orilla1.append(x)
+            if busquedaProfundidad([copia_orilla1,copia_orilla2,"Regresamos a ",x],estado_final,cambiosentido,copia_profundidad):
+              return True
+        if sentido == True:
+          cambiosentido = False
+          for x in orilla2:
+            copia_orilla2 = copy.copy(orilla2)
+            copia_orilla2.remove(x)
+            for y in copia_orilla2:
+              copia2_orilla2 = copy.copy(orilla2)
+              copia_orilla1 = copy.copy(orilla1)
+              copia2_orilla2.remove(x)
+              copia2_orilla2.remove(y)
+              copia_orilla1.append(x)
+              copia_orilla1.append(y)
+              if busquedaProfundidad([copia_orilla1,copia2_orilla2,"Regresamos a ",x,y],estado_final,cambiosentido,copia_profundidad):
+                return True
+        List.pop()      
+    return False
 
-        if OnePiece(orilla2,estado_final):
-            return True
-
-        else:
-            if sentido==False:
-                cambiosentido= True
-                for x in orilla1:
-                    copia_orilla1= copy.copy(orilla1)
-                    copia_orilla1.remove(x)
-                    for y in copia_orilla1:
-                        copia2_orilla1 = copy.copy(orilla1)
-                        copia_orilla2 = copy.copy(orilla1)
-                        copia2_orilla1.remove(x)
-                        copia2_orilla1.remove(y)
-                        copia_orilla2.append(x)
-                        copia_orilla2.append(y)
-                        if busquedaProfundidad([copia2_orilla1,copia_orilla2], estado_final,cambiosentido, copia_profundidad):
-                            return True
-            if sentido==True:
-                cambiosentido= False
-                for x in orilla1:
-                    copia_orilla1 = copy.copy(orilla1)
-                    copia_orilla2 = copy.copy(orilla2)
-                    copia_orilla1.remove(x)
-                    copia_orilla1.append(x)
-                    if busquedaProfundidad([copia_orilla1,copia_orilla2], estado_final,cambiosentido, copia_profundidad):
-                        return True
-            if sentido==True:
-                cambiosentido= False
-                for x in orilla1:
-                    copia_orilla2= copy.copy(orilla2)
-                    copia_orilla2.remove(x)
-                    for y in copia_orilla2:
-                        copia2_orilla2 = copy.copy(orilla2)
-                        copia_orilla1 = copy.copy(orilla1)
-                        copia2_orilla2.remove(x)
-                        copia2_orilla2.remove(y)
-                        copia_orilla1.append(x)
-                        copia_orilla1.append(y)
-                        if busquedaProfundidad([copia_orilla1,copia_orilla2], estado_final,cambiosentido, copia_profundidad):
-                          return True
-
-            List.pop()
-        return False
-
-if  busquedaProfundidad(estInicio, estFinal,Ida, Prof):
-    print("Llegaste a Laugh Tale")
-    for a in List:
-        print(a)
+if busquedaProfundidad(estInicio,estFinal,ida,profundidad):
+  print("Llegaste a Laugh Tale")
+  for a in List:
+    print(a)
+else:
+  print("Aun esta Barbanegra")  
