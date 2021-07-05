39ce7ae899500457fa9157040c1ef6f09bb5afbc
Emilian45 <emilian.muppers@gmail.com>
2021-07-05 18:25:29 -0500

    Funciones

--- Canibales_Profundidad.py
+++ Canibales_Profundidad.py
@@ -17,3 +17,48 @@ estFinal= ["Canibal","Canibal","Canibal","Misionero","Misionero","Misionero"]
 ListaAxiliar=[]
 List=[]
 Ida=False
+
+'''
+Si la orilla destino, el elemento 2 de nuestra lista de listas tiene los 6 objetos que diga  con un true que  se ha completado todos los
+objetivos, solo con una pequeña modificacion agregando nuestro toque personal en cuanto a referencias otakus[Nota no servira esto para anchura]
+'''
+def OnePiece(estado_inicial,estado_final):
+    if len(estado_inicial[1])==estado_final:
+        return True
+    else:
+        return False
+
+'''Pues como se tornaron unos problemas con el metodo que se venia manejando y ahora se esta resolviendo de manera en que
+  que para verificar estemos contando el numero de misiones  y canibales en cada orilla pues sera mejor contar la cantidad de cada elemento dentro de cada orilla
+  Aqui retornaremos el numero de canibales para poder hacer las comparaciones despues
+'''
+def BusquedaCanibal(Orilla):
+  Canibal  = 0
+  for x in Orilla:
+    if x == "Canibal":
+      Canibal += 1 
+  return Canibal
+
+'''Pues como se tornaron unos problemas con el metodo que se venia manejando y ahora se esta resolviendo de manera en que
+  que para verificar estemos contando el numero de misiones  y canibales en cada orilla pues sera mejor contar la cantidad de cada elemento dentro de cada orilla
+  Aqui retornaremos el numero de Misioneros para poder hacer las comparaciones despues
+'''
+def BusquedaMisionero(Orilla):
+  Misionero = 0
+  for x in Orilla:
+    if x == "Misionero":
+      Misionero += 1
+  return Misionero  
+
+
+'''Que retorne  un True si en la orilla que digamos exista el objeto que igual digamos para estar viendo asi hacer las reglas
+'''
+def validacionExisObjeto(Orilla,Objeto):
+  for x in Orilla:
+    if x == Objeto:
+      Objeto +=1
+  return Objeto
+
+  #Aqui ya que tenemos los if solo seria quitar la parte de recursividad porque en si son las reglas 
+
+
