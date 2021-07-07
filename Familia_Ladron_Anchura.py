9e71338ad9a6273167a3b78262fb2fbbe233f444
Emilian45 <emilian.muppers@gmail.com>
2021-07-06 22:39:55 -0500

    Inicio

--- Familia_Ladron_Anchura.py
+++ Familia_Ladron_Anchura.py
@@ -1,1 +1,12 @@
 #Si dios quiere acabo osi osi o no duermo pero si saleeee como que no
+import copy
+import time
+
+estInicio = [[  [["Policia","Ladron","Padre","Hijo","Hijo","Madre","Hija","Hija"],[]]   ]]
+estFinal   = ["Policia","Ladron","Padre","Hijo","Hijo","Madre","Hija","Hija"]
+numero = 0
+direccion = 0
+List = []
+
+
+
